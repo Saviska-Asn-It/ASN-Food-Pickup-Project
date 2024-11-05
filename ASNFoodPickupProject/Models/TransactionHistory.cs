a0be4834411ed1731a4a6f35b78f589c64e02e81
Saviska-Asn-It <saviska@asnit.lk>
2024-11-05 16:45:04 +0530

    Updates

---
+++ ASNFoodPickupProject/Models/TransactionHistory.cs
@@ -0,0 +1,15 @@
+﻿namespace ASNFoodPickupProject.Models
+{
+    public class TransactionHistory
+    {
+        public int TransactionId { get; set; }
+        public DateTime TransactionDate { get; set; }
+        public string TransactionType { get; set; }
+        public decimal Amount { get; set; }
+        public string PaymentMethod { get; set; }
+        public string Status { get; set; }
+        public string OrderId { get; set; }
+        public string RestaurantName { get; set; }
+        public string PromoCode { get; set; }
+    }
+}
