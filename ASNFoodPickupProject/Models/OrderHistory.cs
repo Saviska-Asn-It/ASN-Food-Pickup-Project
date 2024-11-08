79ec5d4d086e739542753cbe23885e52d4be41e1
Saviska Jayawickrama <158861381+Saviska-Jay@users.noreply.github.com>
2024-11-08 06:55:59 +0530

    Some Updates

---
+++ ASNFoodPickupProject/Models/OrderHistory.cs
@@ -0,0 +1,13 @@
+﻿namespace ASNFoodPickupProject.Models
+{
+    public class OrderHistory
+    {
+        public int OrderId { get; set; }
+        public int TransactionId { get; set; }
+        public DateTime OrderDate { get; set; }
+        public string FoodItem { get; set; }
+        public string RestaurantName { get; set; }
+        public int Quantity { get; set; }
+        public decimal Amount { get; set; }
+    }
+}
--- ASNFoodPickupProject/Models/TransactionHistory.cs
+++ ASNFoodPickupProject/Models/TransactionHistory.cs
@@ -4,12 +4,9 @@
     {
         public int TransactionId { get; set; }
         public DateTime TransactionDate { get; set; }
-        public string TransactionType { get; set; }
+        public string FoodItem { get; set; }
         public decimal Amount { get; set; }
         public string PaymentMethod { get; set; }
-        public string Status { get; set; }
-        public string OrderId { get; set; }
         public string RestaurantName { get; set; }
-        public string PromoCode { get; set; }
     }
 }
